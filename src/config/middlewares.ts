a037bb2bc833c4f0ae0bbd8bf3382d5bb0e164a5
Dimitris Ragkousis <d.ragkousis@gmail.com>
2024-12-01 22:57:02 +0200

    Added plugins

---
+++ src/config/middlewares.ts
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'script-src': ['https://cdn.ckeditor.com']
+        },
+      },
+    },
+  },]
