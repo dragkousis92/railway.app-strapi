2d6eebcb4448e79e6c47dd8187d27f570a07c550
Dimitris Ragousis <d.ragkousis@gmail.com>
2024-12-04 13:28:04 +0200

    Update middlewares.ts

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -1,12 +1,14 @@
 export default [
-  {
+{
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'script-src': ['https://cdn.ckeditor.com']
+          'script-src': ['https://cdn.ckeditor.com'],
+          'connect-src': ['https://proxy-event.ckeditor.com']
         },
       },
     },
-  },]
+  }
+,]
